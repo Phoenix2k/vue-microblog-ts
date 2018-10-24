6e0796a56c4c18ce0392c7c2330b3f09011e4c93
Niko Pettersen <niko.pettersen@gmail.com>
2018-10-24 18:33:03 +0300

    Add tests for `Home` page

--- tests/unit/home.spec.ts
+++ tests/unit/home.spec.ts
@@ -9,7 +9,48 @@ describe( 'Home.vue', () => {
 	const wrapper = mount( Home );
 
 	it( 'contains the posts wrapper', () => {
+		wrapper.setData( { error: false, loading: false } );
 		expect( wrapper.contains( PostFeed ) ).toBe( true );
+	} );
+
+	it( 'does not contain the posts wrapper', () => {
+		wrapper.setData( { error: false, loading: true } );
+		expect( wrapper.contains( PostFeed ) ).toBe( false );
+	} );
+
+	it( 'does not contain the posts wrapper', () => {
+		wrapper.setData( { error: true, loading: false } );
+		expect( wrapper.contains( PostFeed ) ).toBe( false );
+	} );
+
+	it( 'does not contain the error wrapper', () => {
+		wrapper.setData( { error: true, loading: true } );
+		expect( wrapper.find( '.error' ).exists() ).toBe( false );
+	} );
+
+	it( 'does not contain the error wrapper', () => {
+		wrapper.setData( { error: false, loading: false } );
+		expect( wrapper.find( '.error' ).exists() ).toBe( false );
+	} );
+
+	it( 'contains the error wrapper', () => {
+		wrapper.setData( { error: true, loading: false } );
+		expect( wrapper.find( '.error' ).exists() ).toBe( true );
+	} );
+
+	it( 'contains the loading wrapper', () => {
+		wrapper.setData( { loading: true, error: false } );
+		expect( wrapper.find( '.loading' ).exists() ).toBe( true );
+	} );
+
+	it( 'does not contain the loading wrapper', () => {
+		wrapper.setData( { error: true, loading: true } );
+		expect( wrapper.find( '.loading' ).exists() ).toBe( false );
+	} );
+
+	it( 'does not contain the loading wrapper', () => {
+		wrapper.setData( { error: true, loading: false } );
+		expect( wrapper.find( '.loading' ).exists() ).toBe( false );
 	} );
 
 } );
