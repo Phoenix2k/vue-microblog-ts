c192210068123562375d09130aa48240bab4e713
Niko Pettersen <niko.pettersen@gmail.com>
2018-10-26 09:43:29 +0300

    Add global base styles

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="app">
-		<div id="nav">
+		<div class="nav">
 			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
+			<router-link to="/admin">Admin</router-link>
 		</div>
 		<router-view/>
 	</div>
@@ -18,15 +18,14 @@ export default class App extends Vue {
 	}
 }
 </script>
-<style lang="scss">
+<style scoped lang="scss">
+@import url( 'scss/base.scss' );
+
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
 }
-#nav {
+
+.nav {
 	padding: 30px;
 	a {
 		font-weight: bold;
