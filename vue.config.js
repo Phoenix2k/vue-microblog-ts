fd67f214f3681b9739f1a98c89206a824908bf60
Niko Pettersen <niko.pettersen@gmail.com>
2018-10-28 12:43:25 +0200

    Move generated files to the `dist` folder

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
-// Prevent copy plugin from failing if `public` folder doesn't exist
-try { require( 'fs' ).mkdirSync( './public' ); } catch ( error ) { };
-
 module.exports = {
 	baseUrl: process.env.BASE_URL,
-	chainWebpack: config => {
-		config.plugin( 'copy' ).tap( ( [ options ] ) => {
-			options[0].from = 'src/client/public';
-		} );
-	},
 	devServer: {
 		overlay: {
 			errors: true,
@@ -21,12 +13,12 @@ module.exports = {
 		}
 	},
 	lintOnSave: 'production' !== process.env.NODE_ENV,
-	outputDir: './public',
+	outputDir: './dist/public',
 	pages: {
 		index: {
 			entry: './src/client/main.ts',
 			filename: 'index.html',
-			template: './src/client/public/index.html'
+			template: './public/index.html'
 		}
 	}
 };
